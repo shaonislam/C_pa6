bfe4184abc3d22a0f079b0eb7dc559015407375d
Shaon Islam <sidw8@mail.umsl.edu>
2017-12-08 18:17:17 -0600

    More strings were added on top of what was originally given to make it look like how it should

--- testsim.c
+++ testsim.c
@@ -4,18 +4,24 @@ int main(int argc, char *argv[])
 {
 	int n;
 	int sec;
-	
+
 	if (argc <= 2) 
-	{	
-		fprintf(stderr, “Usage: %s sleep_time repeat_factor\n”, argv[0]);
+	{
+		fprintf(stderr, "Usage: %s sleep_time repeat_factor\n", argv[0]);
 		exit(1);
 	}
-	
-	sscanf(argv[1], “%d”, &sec);
-	sscanf(argv[2], “%d”, &n);
-	while (n-- > 0)
+
+	sscanf(argv[1], "%d", &sec);
+	sscanf(argv[2], "%d", &n);
+
+
+	while (n > 0)
+	{
 		sleep(sec);
-	
-	printf(“Process ID = %d \n”, getpid());
-	return sec;
+		printf("Process %d [%d] is sleeping for %d seconds\n", getpid(),n,sec);
+		n--;
+	}
+
+	printf("Process %d end!\n", getpid());
+	return 0;
 }
